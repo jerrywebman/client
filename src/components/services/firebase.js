4cd4f05b6ca49683fcb661bae5f1f4fcb698082c
jerrywebman <jerrycifeanyi@gmail.com>
2020-06-16 07:13:27 -0700

    netlify host

--- src/components/services/firebase.js
+++ src/components/services/firebase.js
@@ -1,21 +1,21 @@
 import firebase from "firebase";
 
 const config = {
-  apiKey: apiKey,
-  authDomain: authDomain,
-  databaseURL: databaseURL,
-  projectId: projectId,
-  storageBucket: storageBucket,
-  messagingSenderId: messagingSenderId,
-  appId: appIdHeroku,
+  // apiKey: apiKey,
+  // authDomain: authDomain,
+  // databaseURL: databaseURL,
+  // projectId: projectId,
+  // storageBucket: storageBucket,
+  // messagingSenderId: messagingSenderId,
+  // appId: appIdHeroku,
 
-  //   apiKey: "AIzaSyBlOxKRglQBCyljw2cuSuBMlR4zShxCd3w",
-  //   authDomain: "coinnewsafrica-react.firebaseapp.com",
-  //   databaseURL: "https://coinnewsafrica-react.firebaseio.com",
-  //   projectId: "coinnewsafrica-react",
-  //   storageBucket: "coinnewsafrica-react.appspot.com",
-  //   messagingSenderId: "161765178638",
-  //   appId: "1:161765178638:web:6960efe055589cbc4f6b5f",
+  apiKey: "AIzaSyBlOxKRglQBCyljw2cuSuBMlR4zShxCd3w",
+  authDomain: "coinnewsafrica-react.firebaseapp.com",
+  databaseURL: "https://coinnewsafrica-react.firebaseio.com",
+  projectId: "coinnewsafrica-react",
+  storageBucket: "coinnewsafrica-react.appspot.com",
+  messagingSenderId: "161765178638",
+  appId: "1:161765178638:web:6960efe055589cbc4f6b5f",
   // };
 };
 
